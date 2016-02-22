cb7c9a767b6099a46d7af6ee0b9995c8604266b1
김기정 <graffity13@gmail.com>
2016-02-22 10:32:48 +0900

    heain.js 수정1

--- HateCouple/src/main/webapp/resources/js/haein.js
+++ HateCouple/src/main/webapp/resources/js/haein.js
@@ -32,19 +32,15 @@ $(document).ready(function() {
 		    		   var items2 = json2.channel.item;
 		   			   var imageSrc = "http://i1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
 						
+		   			   var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+		   			   var options = { //지도를 생성할 때 필요한 기본 옵션
+		   					   center: new daum.maps.LatLng(pos), //지도의 중심좌표.
+		   					   level: 10 //지도의 레벨(확대, 축소 정도)
+		   			   };
+		   			   var map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
+		   			   
+		   			   
 						$.each(items2,function(i,it2){
-							
-							if (i==0) {
-								
-								var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-								var options = { //지도를 생성할 때 필요한 기본 옵션
-									center: new daum.maps.LatLng(it2.latitude,it2.longitude), //지도의 중심좌표.
-									level: 3 //지도의 레벨(확대, 축소 정도)
-								};
-							}
-							
-							var map = new daum.maps.Map(container, options); //지도 생성 및 객체 리턴
-							
 													   
 							    // 마커 이미지의 이미지 크기 입니다
 						   var imageSize = new daum.maps.Size(24, 35); 
