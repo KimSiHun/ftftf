07729ead0e8efbf0cd6e1fba76a3bb5ff6133e36
Woochan <twoseam61@gmail.com>
2016-02-22 14:19:14 +0900

    포스터이미지 클릭시 각 포스터의 고유링크를 가져오는 코드 추가

--- HateCouple/src/main/webapp/WEB-INF/views/movieMain.jsp
+++ HateCouple/src/main/webapp/WEB-INF/views/movieMain.jsp
@@ -5,43 +5,15 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <head>
-<title>jQuery UI Tabs - Default functionality</title>
+<title></title>
 <!-- link to css -->
 <link rel="stylesheet" href="resources/stylesheets/movieMain.css" type="text/css">
-
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="resources/js/movieMain.js" ></script>
-<style type="text/css">
-body {
-	font-family: "Trebuchet MS", "Helvetica", "Arial",  "Verdana", "sans-serif";
-	font-size: 62.5%;
-}
-</style>
 
-
-</head>
 <body>
 <div id = "slideDiv"></div>
-<!-- 
-	<div id="tabs">
-		<ul>
-			<li><a href="#tabs-1">18일</a></li>
-			<li><a href="#tabs-2">19일</a></li>
-			<li><a href="#tabs-3">20일</a></li>
-		</ul>
-		<div id="tabs-1">
-			<p> </p>     
-      </div>
-		<div id="tabs-2">
-			<p></p>
-		</div>
-		<div id="tabs-3">
-			<p></p>
-		</div>
-	</div>
- -->	 
  <table id = "Tb">
  </table>
 </body>
--- HateCouple/src/main/webapp/resources/js/movieMain.js
+++ HateCouple/src/main/webapp/resources/js/movieMain.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){	      
-	      // tabs
-//	$( "#tabs" ).tabs({
-//		beforeLoad: function( event, ui ) {
-//			ui.jqXHR.fail(function() {
-//				ui.panel.html(
-//					"Couldn't load this tab. We'll try to fix this as soon as possible. " +
-//					"If this wouldn't be a demo." );
-//			});
-//		}
-//	});		
+
 		 $.getJSON("getMV", function(json){
 		      // json객체 -> String      
 		      //alert( JSON.stringify(json));		      
@@ -37,13 +28,11 @@ $(document).ready(function(){
 		                         });      
 		            // json으로 제대로 만들어 졌는지 콘솔에서 확인
 		            console.log(JSON.stringify(posterUrl));   
-		           // v=1
+
 		            // posterUrl에서 이미지주소를 슬라이드에 넣어주는 메소드
 		            $.each(posterUrl, function(i, mv){               
 		               var img = $('<img/>').attr('src', mv.uniquePoster).css({'width' : '100px' , 'height' : '100px'});
-		            //   v++
-		             //  str+v;
-		             //  id,'sss'
+		               
 		               $.each(img, function(i, mv){      
 		               var link = $('<a></a>').attr('class', "imgfile").append(img);
 		               var pdiv = $('<div class ="poster" style="display:inline;"></div>').append(link);
@@ -53,27 +42,42 @@ $(document).ready(function(){
 		                   var $giveImgID = $("#slideDiv img");
 		                   $giveImgID.attr('id', function (index) {
 		                       return 'img' + index;
-		                   });
-		                   
+		                   });		                   
 		                   var $giveaID = $("#slideDiv a");
 		                   $giveaID.attr('id', function (index){
 		                	   return 'link' + index;
-		                   });
-		              // $('#tabs-1').append(pdiv);
+		                   });		          
 		           		});  
 		            });   
-		            	// 상단 포스터 클릭 시 하단 테이블에 정보 출력가능하게 하는 클릭 이벤트
-		               $(".imgfile").click(function(){
-		            	   $("#Tb").empty();
-		                $.each(json.movies, function(i, s){	                	
-		                     var title = $('<td></td>').text(s.c_name);
-		                     var loc = $('<td></td>').text(s.c_cine_location);
-		                     var time = $('<td></td>').text(s.c_begin_time);
-		                     var seat = $('<td></td>').text(s.c_clearSeat);		                     
-		                     var tr =$('<tr></tr>').append(title, loc, time, seat);		                     
-		                     $("#Tb").append(tr).css("width","400px");           
-		                    });         
-		               });  
+		            
+		            // 이미지 클릭 시 각 고유 이미지주소를 가져오기 위한 코드
+		            $(function(){
+		                $("img[id^='img'").click(function() {
+		                    var getImgLink =$(this).attr("src");
+		                    
+		                    console.log(getImgLink);
+		                });
+		            });
+
+
+//		            $('[id^=link]','#slideDiv').click(function(){	
+//		            	 alert('click');
+//		            	 $("#Tb").empty();         
+//		            });
+//		            
+		            
+		            // 상단 포스터 클릭 시 하단 테이블에 정보 출력가능하게 하는 클릭 이벤트
+//		               $(".imgfile").click(function(){
+//		            	   $("#Tb").empty();
+//		                $.each(json.movies, function(i, s){	                	
+//		                     var title = $('<td></td>').text(s.c_name);
+//		                     var loc = $('<td></td>').text(s.c_cine_location);
+//		                     var time = $('<td></td>').text(s.c_begin_time);
+//		                     var seat = $('<td></td>').text(s.c_clearSeat);		                     
+//		                     var tr =$('<tr></tr>').append(title, loc, time, seat);		                     
+//		                     $("#Tb").append(tr).css("width","400px");           
+//		                    });         
+//		               });  
 
 		 });
 });		
