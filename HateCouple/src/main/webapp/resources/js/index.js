f1014013f287a05150d5708fdca0b17121a4c5e7
김시훈/FTF팀조장 <paradaise110@hanmail.net>
2016-02-24 19:54:07 +0900

    haein.js index.js에 합쳤습니다.

--- HateCouple/src/main/webapp/resources/js/index.js
+++ HateCouple/src/main/webapp/resources/js/index.js
@@ -4,8 +4,41 @@
 			var addr = document.getElementById('sample6_address').value;
 			document.getElementById('search_addr').value = addr;
 			var dongaddr = document.getElementById('sample6_address').value;
-			document.getElementById('dongName').value = addr;
+
 			$("#fst").slideUp("slow");
+			
+			
+			
+			var url = "https://apis.daum.net/local/geo/addr2coord?apikey=085c621120828d233c8a25e9eee763e8&q="+addr+"&output=json";
+			// 좌표 구하기
+			$.getJSON(url+"&callback=?", function(json){
+				// 초기화시 리스트 비움.
+				$("ul").empty();
+								
+				var items = json.channel.item;
+				// 좌표를 검색형태로 가공
+				$.each(items, function(i, it){
+					var lat = it.lat;
+					var lng = it.lng;
+					
+					var pos = lat+","+lng;
+					// 가공된 좌표, 검색질의로 해당 질의에 답 구하기.
+					var url2="https://apis.daum.net/local/v1/search/keyword.json?apikey=085c621120828d233c8a25e9eee763e8&q&query=CGV영화관&location="+pos+"&radius=2000";
+					$.getJSON(url2+"&callback=?",function(json2){
+						var items2 = json2.channel.item;
+						// 해당 json value로 li에 붙이기 . -> 영화관 이름만
+						$.each(items2,function(i,it2){
+							
+							// list에 영화관 이름 붙이기. 상위 ul
+							var li = $("<li></li>").append(it2.title);
+							$("ul").append(li);
+						});
+					});
+					
+				});
+				
+			});
+			
 		});
 		
 		// 최초 구역으로 이동 - 기존 주소창 비우기
